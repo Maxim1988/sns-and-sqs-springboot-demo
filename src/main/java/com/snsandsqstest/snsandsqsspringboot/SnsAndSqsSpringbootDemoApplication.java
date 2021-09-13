ab7728e0bc0f97a24c8e76bd98eef3f500672bd2
maximzvyagin <Max198805@yandex.ru>
2021-09-13 12:03:58 +0300

    master first commit

---
+++ src/main/java/com/snsandsqstest/snsandsqsspringboot/SnsAndSqsSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.snsandsqstest.snsandsqsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsAndSqsSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsAndSqsSpringbootDemoApplication.class, args);
+	}
+
+}
